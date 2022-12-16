c6fb9b0fd19dbdf6f13559475242b7559e64752b
Nur Rohman <92500812+nurMangg@users.noreply.github.com>
2022-12-16 22:26:38 +0700

    calculator

---
+++ src/calculator/Methods.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculator;
+
+/**
+ *
+ * @author mangg
+ * Kelas : 3D
+ * NIM : 21090118
+ */
+public class Methods {
+    public static double Perhitungan(String pilihan, double operand1, double operand2){
+        switch (pilihan) {
+            case "+":
+                return operand1 + operand2;
+            case "-":
+                return operand1 - operand2;
+            case "*":
+                return operand1 * operand2;
+            case "/":
+                return operand1 / operand2;
+            default:
+                return 0;
+        }
+    }
+}
+
+
